c7abf09ddc9a61f8c87e5bb7d5cc2c446aef28bb
Eirik Kvalheim <eirik.kvalheim@dinerp.no>
2017-09-28 16:10:52 +0800

    updated readme, added an outline for user management in the same huge elm file. Next up is application structure

--- Main.elm
+++ Main.elm
@@ -10,8 +10,28 @@ import String
 -- MODEL
 
 
+testDataUsers : List User
+testDataUsers =
+    [ { uid = 1, username = "oracle", email = "oracle@oracle.com" }
+    , { uid = 2, username = "varjen", email = "varjen@varjen.com" }
+    , { uid = 3, username = "budull", email = "budull@budull.com" }
+    ]
+
+
+testDataWishes : List Wish
+testDataWishes =
+    [ { uid = 1, isBought = False, description = "Bicycle", url = "http://greatbikes.com", userId = 1 }
+    , { uid = 2, isBought = False, description = "Sweater", url = "http://greatsweaters.com", userId = 1 }
+    , { uid = 3, isBought = False, description = "Giftcard @ amazon", url = "http://amazon.com", userId = 1 }
+    , { uid = 4, isBought = False, description = "Lamp", url = "http://greatlamps.com", userId = 1 }
+    , { uid = 5, isBought = False, description = "China", url = "http://greatchina.com", userId = 2 }
+    ]
+
+
 type alias Model =
-    { draft : Wish
+    { draftUser : User
+    , users : List User
+    , draftWish : Wish
     , wishes : List Wish
     }
 
@@ -21,6 +41,14 @@ type alias Wish =
     , isBought : Bool
     , description : String
     , url : String
+    , userId : Int
+    }
+
+
+type alias User =
+    { uid : Int
+    , username : String
+    , email : String
     }
 
 
@@ -31,8 +59,10 @@ init =
 
 emptyModel : Model
 emptyModel =
-    { draft = draftWish
-    , wishes = []
+    { draftWish = draftWish
+    , wishes = testDataWishes
+    , users = testDataUsers
+    , draftUser = draftUser
     }
 
 
@@ -42,7 +72,29 @@ draftWish =
     , isBought = False
     , url = ""
     , uid = -1
+    , userId = -1
     }
+
+
+draftUser : User
+draftUser =
+    { uid = -1
+    , username = ""
+    , email = ""
+    }
+
+
+newUser : Int -> String -> String -> User
+newUser uid_ username_ email_ =
+    { username = username_
+    , email = email_
+    , uid = uid_
+    }
+
+
+isUserValid : User -> Bool
+isUserValid user =
+    String.isEmpty user.username || String.isEmpty user.email
 
 
 newWish : Int -> String -> String -> Wish
@@ -51,6 +103,7 @@ newWish uid_ desc_ url_ =
     , isBought = False
     , url = url_
     , uid = uid_
+    , userId = 1
     }
 
 
@@ -64,20 +117,31 @@ isWishInvalid wish =
 
 
 type Msg
-    = Add
-    | PrepareAdd Int
+    = NoOp
+    | AddWish
+    | CreateWish Int
     | UpdateDraftDesc String
     | UpdateDraftUrl String
-    | Delete Int
+    | UpdateWish Int Wish
+    | UpdateWishDescription Int String
+    | UpdateWishUrl Int String
+    | DeleteWish Int
+    | AddUser
+    | CreateUser Int
+    | UpdateDraftUserName String
+    | UpdateDraftUserEmail String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        PrepareAdd uid ->
+        NoOp ->
+            ( model, Cmd.none )
+
+        CreateWish uid ->
             let
                 wish =
-                    model.draft
+                    model.draftWish
             in
                 ( { model
                     | wishes =
@@ -85,7 +149,7 @@ update msg model =
                             model.wishes
                         else
                             model.wishes ++ [ newWish uid wish.description wish.url ]
-                    , draft = draftWish
+                    , draftWish = draftWish
                   }
                 , Cmd.none
                 )
@@ -93,13 +157,13 @@ update msg model =
         UpdateDraftDesc desc ->
             let
                 currentDraft =
-                    model.draft
+                    model.draftWish
 
                 newDraft =
                     { currentDraft | description = desc }
             in
                 ( { model
-                    | draft = newDraft
+                    | draftWish = newDraft
                   }
                 , Cmd.none
                 )
@@ -107,21 +171,90 @@ update msg model =
         UpdateDraftUrl url ->
             let
                 currentDraft =
-                    model.draft
+                    model.draftWish
 
                 newDraft =
                     { currentDraft | url = url }
             in
                 { model
-                    | draft = newDraft
+                    | draftWish = newDraft
                 }
                     ! []
 
-        Delete uid ->
+        UpdateWish uid wish ->
+            let
+                updateWish w =
+                    if w.uid == uid then
+                        { w | description = wish.description, url = wish.url }
+                    else
+                        w
+            in
+                ( { model | wishes = List.map updateWish model.wishes }, Cmd.none )
+
+        UpdateWishDescription uid desc ->
+            let
+                updateWish w =
+                    if w.uid == uid then
+                        { w | description = desc }
+                    else
+                        w
+            in
+                ( { model | wishes = List.map updateWish model.wishes }, Cmd.none )
+
+        UpdateWishUrl uid url ->
+            let
+                updateWish w =
+                    if w.uid == uid then
+                        { w | url = url }
+                    else
+                        w
+            in
+                ( { model | wishes = List.map updateWish model.wishes }, Cmd.none )
+
+        DeleteWish uid ->
             ( { model | wishes = List.filter (\t -> t.uid /= uid) model.wishes }, Cmd.none )
 
-        Add ->
-            ( model, Random.generate PrepareAdd (Random.int 1 100) )
+        AddWish ->
+            ( model, Random.generate CreateWish (Random.int 1 100) )
+
+        CreateUser uid ->
+            let
+                currentDraftUser =
+                    model.draftUser
+            in
+                ( { model
+                    | users =
+                        if isUserValid currentDraftUser then
+                            model.users
+                        else
+                            model.users ++ [ newUser uid currentDraftUser.username currentDraftUser.email ]
+                    , draftUser = draftUser
+                  }
+                , Cmd.none
+                )
+
+        AddUser ->
+            ( model, Random.generate CreateUser (Random.int 200 300) )
+
+        UpdateDraftUserName username ->
+            let
+                draftUser =
+                    model.draftUser
+
+                updatedUser =
+                    { draftUser | username = username }
+            in
+                ( { model | draftUser = updatedUser }, Cmd.none )
+
+        UpdateDraftUserEmail email ->
+            let
+                draftUser =
+                    model.draftUser
+
+                updatedUser =
+                    { draftUser | email = email }
+            in
+                ( { model | draftUser = updatedUser }, Cmd.none )
 
 
 
@@ -139,40 +272,125 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
+    div [ style [ ( "padding", "20px" ) ] ]
+        [ viewWishes model
+        , viewUsers model
+        ]
+
+
+viewWishes : Model -> Html Msg
+viewWishes model =
     div
-        [ style [ ( "padding", "20px" ) ] ]
+        [ style [] ]
         [ h1 []
-            [ text "Wishlist Application" ]
+            [ text "Wishlist Management" ]
         , p []
             [ text "Create a new wish"
             ]
         , input
             [ placeholder "Description"
             , onInput UpdateDraftDesc
-            , value model.draft.description
+
+            -- , value model.draft.description
             ]
             []
         , input
             [ placeholder "URL"
             , onInput UpdateDraftUrl
-            , value model.draft.url
             ]
             []
-        , button [ onClick Add ]
+        , button [ onClick AddWish ]
             [ text "Add Wish" ]
         , p []
             [ text "Your wishes"
             ]
-        , ul []
+        , ul
+            [ style
+                [ ( "list-style-type", "none" )
+                , ( "padding", "0" )
+                , ( "margin", "0" )
+                ]
+            ]
             (List.map viewWish model.wishes)
+        , div [ style [ ( "margin-top", "60px" ) ] ]
+            [ text (toString model.wishes)
+            , text (toString model.draftWish)
+            ]
         ]
 
 
 viewWish : Wish -> Html Msg
 viewWish wish =
     li []
-        [ text (toString wish.uid ++ ", " ++ wish.description ++ ", " ++ wish.url)
-        , button [ onClick (Delete wish.uid) ]
+        [ input
+            [ placeholder "Description"
+            , value wish.description
+            , onInput (UpdateWishDescription wish.uid)
+            ]
+            []
+        , input
+            [ placeholder "URL"
+            , value wish.url
+            ]
+            []
+        , text ("ID: " ++ toString wish.uid)
+        , button [ onClick (DeleteWish wish.uid) ]
+            [ text "Delete " ]
+        ]
+
+
+viewUsers : Model -> Html Msg
+viewUsers model =
+    div
+        [ style [] ]
+        [ h1 []
+            [ text "User Management" ]
+        , p []
+            [ text "Create a new user"
+            ]
+        , input
+            [ placeholder "Username"
+            , onInput UpdateDraftUserName
+            ]
+            []
+        , input
+            [ placeholder "email"
+            , onInput UpdateDraftUserEmail
+            ]
+            []
+        , button [ onClick AddUser ]
+            [ text "Add user" ]
+        , p []
+            [ text "All users"
+            ]
+        , ul
+            [ style
+                [ ( "list-style-type", "none" )
+                , ( "padding", "0" )
+                , ( "margin", "0" )
+                ]
+            ]
+            (List.map viewUser model.users)
+        , div [ style [ ( "margin-top", "60px" ) ] ]
+            [ text (toString model.users) ]
+        ]
+
+
+viewUser : User -> Html Msg
+viewUser user =
+    li []
+        [ input
+            [ placeholder "Username"
+            , value user.username
+            ]
+            []
+        , input
+            [ placeholder "Email"
+            , value user.email
+            ]
+            []
+        , text ("UserId: " ++ toString user.uid)
+        , button [ onClick NoOp ]
             [ text "Delete " ]
         ]
 
--- README.md
+++ README.md
@@ -11,5 +11,56 @@ The plan is to make a wishlist application in elm..
 
  ## Resources going forward
 
+Todo app
  * https://github.com/evancz/elm-todomvc/blob/master/Todo.elm
+
+
+ Awesome elm
  * https://github.com/isRuslan/awesome-elm
+ 
+
+ Application structure
+* https://becoming-functional.com/nine-guidelines-for-modular-elm-development-fe18d2f7885e
+
+* http://blog.jenkster.com/2016/04/how-i-structure-elm-apps.html
+
+
+
+## WIP
+* Create two modules, one for Wish and one for User
+
+
+## Feature list
+
+### Wish management
+* Read, Create , Update , Delete
+
+### User management
+* Read, Create, Update , Delete
+
+
+
+### Router
+* Need to support routing (client side vs server side )
+* /User routes, Basically all protected client side routing is in the context of the user
+* / -> landing page
+* /about -> who created the service
+* /faq -> how to use it
+
+### Authentication
+* Need to support jwt auth
+
+### Serverside
+* Can build it with node.js (what about go ?)
+* Serve data from server (wishes and users  - REST api)
+    * /Users (GET and POST)
+    * /Wishes (GET and POST)
+* Persist data to db
+
+### Data model
+|| User || 1 - * || Wish ||
+
+
+## Flows
+ * Add a user by specifying username and email
+    * Then navigate to user profile page
